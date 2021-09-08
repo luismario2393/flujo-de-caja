11cb5204a18b403fb4e98f07929cb44cd364ff83
LUIS MARIO AMOR SANCHEZ <65006606+luismario2393@users.noreply.github.com>
2021-09-07 19:52:35 -0500

    Funcionalidades terminadas faltan detalles

--- js/app.js
+++ js/app.js
@@ -8,6 +8,11 @@ const title = document.querySelector('#title-form');
 const titleSpan = document.querySelector('#title-form span');
 const pFromE = document.querySelector('#p-form-e');
 const pFromC = document.querySelector('#p-form-c');
+const inputTipo = document.querySelector('#tipo');
+const inputValor = document.querySelector('#valor');
+let ingreso = [];
+let gasto = [];
+
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -16,14 +21,203 @@ document.addEventListener('DOMContentLoaded', () => {
 
   btnFormCan.addEventListener('click', e => {
     e.preventDefault();
-
     containerForm.classList.remove('active');
     form.classList.remove('active')
-  })
+    form.reset();
+  });
+
+  inputTipo.addEventListener('blur', validarForm);
+  inputValor.addEventListener('blur', validarForm);
+  
+  form.addEventListener('submit', leerInfo)
+  
 
 });
 
 // Funciones
+
+function leerInfo(e) {
+  e.preventDefault();
+  
+  if(titleSpan.textContent === 'Ingreso') {
+
+    infoIngreso = {
+      tipo: document.querySelector('#tipo').value,
+      valor: parseInt(document.querySelector('#valor').value),
+      id: Date.now()
+    }
+  
+    ingreso = [...ingreso, infoIngreso];
+  
+    ingresoHTML()
+    form.reset();
+    containerForm.classList.remove('active');
+    
+  } else if(titleSpan.textContent === 'Gasto'){
+
+    infoGasto = {
+      tipo: document.querySelector('#tipo').value,
+      valor: parseInt(document.querySelector('#valor').value),
+      id: Date.now()
+    }
+
+    gasto = [...gasto, infoGasto];
+
+    gastoHTML();
+    form.reset();
+    containerForm.classList.remove('active');
+    
+  }
+
+  
+  const ingresoReducer = ingreso.reduce((total, ingreso) => total + ingreso.valor, 0);
+  const gastoReducer = gasto.reduce((total, gasto) => total + gasto.valor, 0);
+  const balance = ingresoReducer - gastoReducer;
+  
+  if(balance < 0) {
+    alertaBalance('Debes estar más pendiente a tus cifras estas en negativo', 'error');
+  } else if (balance === 0) {
+    alertaBalance('Estamos en cero', 'neutro');
+  } else {
+    alertaBalance('Tus finanzas estan sanas', 'positivo')
+  }
+
+  const valor = document.querySelector('#balance');
+
+  while(valor.firstChild) {
+    valor.removeChild(valor.firstChild)
+  }
+  const span = document.createElement('span');
+  span.textContent = balance;
+
+  valor.appendChild(span);
+  
+}
+
+function alertaBalance(msg, tipo) {
+  
+  const divMensaje = document.querySelector('#mensajito');
+  const mensaje = document.createElement('p');
+
+  
+  if(tipo === 'error') {
+    mensaje.remove()
+    mensaje.classList.remove('neutro', 'positivo' );
+    mensaje.classList.add('mensajito', 'error' );
+    mensaje.textContent = msg;
+  } else if(tipo === 'neutro') {
+    mensaje.remove()
+    mensaje.classList.remove('error', 'positivo' );
+    mensaje.classList.add('mensajito', 'neutro' );
+    mensaje.textContent = msg;
+  } else {
+    mensaje.remove()
+    mensaje.classList.add('mensajito', 'positivo' );
+    mensaje.classList.add('error', 'neutro' );
+    mensaje.textContent = msg;
+  }
+
+  while(divMensaje.firstChild) {
+    divMensaje.removeChild(divMensaje.firstChild)
+  }
+  
+    
+  
+  divMensaje.appendChild(mensaje);
+    
+  
+}
+
+
+function gastoHTML() {
+  const listaGasto = document.querySelector('#lista-gasto');
+  const lista = document.createElement('li');
+  lista.classList.add('item');
+
+  gasto.forEach(g => {
+    const { tipo, valor } = g;
+    lista.innerHTML = `
+      <p>${tipo}</p>
+      <p>$-${valor}</p>
+    `;
+    listaGasto.appendChild(lista);
+  });
+
+  const totalGasto = document.querySelector('#total-gasto');
+  const totalGastoSpan = document.createElement('span');
+  
+  while(totalGasto.firstChild) {
+    totalGasto.removeChild(totalGasto.firstChild)
+  }
+  const gastoReducer = gasto.reduce((total, gasto) => total + gasto.valor, 0);
+  totalGastoSpan.textContent = `$-${gastoReducer}`;
+  totalGasto.appendChild(totalGastoSpan);
+  
+}
+
+function ingresoHTML() {
+  const listaIngreso = document.querySelector('#lista-ingreso');
+  const lista = document.createElement('li');
+  lista.classList.add('item')
+
+  ingreso.forEach(i => {
+    const { tipo, valor } = i
+    lista.innerHTML = `
+        <p>${tipo}</p>
+        <p>$${valor}</p>
+    `;
+    listaIngreso.appendChild(lista);
+
+  });
+
+  const totalIngreso = document.querySelector('#total-ingreso');
+  const totalIngresoSpan = document.createElement('span');
+
+  while(totalIngreso.firstChild) {
+    totalIngreso.removeChild(totalIngreso.firstChild)
+  }
+  const ingresoReducer = ingreso.reduce((total, ingreso) => total + ingreso.valor, 0);
+  totalIngresoSpan.textContent = `$ ${ingresoReducer}`;
+  totalIngreso.appendChild(totalIngresoSpan);
+
+  
+}
+
+function balance() {
+
+  
+}
+ 
+function validarForm(e) {
+  e.preventDefault();
+  const inputTipo = document.querySelector('#tipo').value;
+  const inputValor = document.querySelector('#valor').value;
+  
+  if(inputTipo === '' || inputValor === '') {
+    mostrarMensaje('Todos los campos son obligatorios')
+  }
+  
+}
+
+function mostrarMensaje(msg) {
+
+  const alerta = document.querySelector('.alerta');
+  const mensaje = document.createElement('p');
+
+  if(!alerta) {
+
+    mensaje.classList.add('alerta');
+    mensaje.textContent = msg;
+  
+    form.appendChild(mensaje);
+  }
+
+  setTimeout(() => {
+    mensaje.remove()
+  }, 2000);
+
+}
+
 
 function mostrarVentanaIngreso() {
   const btnFromSpan = document.querySelector('#btn-form span');
